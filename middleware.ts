a8161b895e8d49b7fa81ea9c26a21d1cfec3fa12
Sharli-251104 <mail2sharli@gmail.com>
2024-04-12 11:28:34 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,14 @@
-import { authMiddleware, /* createRouteMatcher */ } from "@clerk/nextjs";
-/* const protectedRoutes = createRouteMatcher([
-    '/',
-    '/upcoming',
-    '/previous',
-    '/recordings',
-    '/personal-room',
-    '/meeting(.*)'
-]) */
-export default authMiddleware((auth, req)=>{
-if(req) auth().protect();
+import { AuthMiddleware, authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware((auth: AuthMiddleware | undefined, req: any) => {
+    if (auth && req) {
+        auth().protect();
+    }
 });
+
 export const config = {
-  matcher: [
-    "/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"]
+    matcher: [
+        "/((?!.+\\.[\\w]+$|_next).*)",
+        "/(api|trpc)(.*)"
+    ]
 };
