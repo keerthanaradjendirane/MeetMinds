574fea9d84ceab2dae16cac78b2fb83670e4d32e
Sharli-251104 <mail2sharli@gmail.com>
2024-04-12 11:45:35 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,12 @@
 import { authMiddleware } from "@clerk/nextjs";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default authMiddleware((req: NextApiRequest, res: NextApiResponse) => {
-    const { auth } = req;
-    if (auth) {
-        auth().protect();
-    }
+export default authMiddleware(() => {
+    return async (req: NextApiRequest, res: NextApiResponse) => {
+        if (req.auth) {
+            await req.auth().protect();
+        }
+    };
 });
 
 export const config = {
